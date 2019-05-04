c3b75f36130bcf8abe2be76f2d4025914bea09d6
Lekha Surasani <lesurasani@gmail.com>
2019-05-04 08:18:32 -0500

    Updates

---
+++ precache-manifest.e821d9875082565f59d9bdca027c58a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9f0ef9c3183c18fd73f9b242c1ab23",
+    "url": "/react-pwa-example/index.html"
+  },
+  {
+    "revision": "630984115798ec627b03",
+    "url": "/react-pwa-example/static/css/main.6b1cf02d.chunk.css"
+  },
+  {
+    "revision": "fadbb0705bdd11d4fb8f",
+    "url": "/react-pwa-example/static/js/2.5d907561.chunk.js"
+  },
+  {
+    "revision": "630984115798ec627b03",
+    "url": "/react-pwa-example/static/js/main.d12f9b84.chunk.js"
+  },
+  {
+    "revision": "adaa4f14feb1b0cc553f",
+    "url": "/react-pwa-example/static/js/runtime~main.af49c7ea.js"
+  }
+]);
